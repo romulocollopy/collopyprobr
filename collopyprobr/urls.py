aa9bda9c1ad29641090168e71808ec0ce3b33d30
romulocollopy <romulocollopy@gmail.com>
2014-02-03 00:13:50 -0200

    update ignored files

--- collopyprobr/urls.py
+++ collopyprobr/urls.py
@@ -3,10 +3,9 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-from collopyprobr.core.views import HomeView
 
-urlpatterns = patterns('',
-        url(r'^$', HomeView.as_view(),name='home'),
+urlpatterns = patterns('collopyprobr.core.views',
+        url(r'^$', 'homepage', name='home'),
         # url(r'^blog/', include('blog.urls')),
         url(r'^admin/', include(admin.site.urls)),
 )
