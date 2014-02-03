a11d0c7cc88a42fc4fd9f0e1acee48abfad81503
romulocollopy <romulocollopy@gmail.com>
2014-02-03 17:48:27 -0200

    Adicionados testes de rotas e templates Adicionado fakecontent para views

--- collopyprobr/core/tests.py
+++ collopyprobr/core/tests.py
@@ -1,3 +1,42 @@
+# coding: utf-8
 from django.test import TestCase
 
-# Create your tests here.
+class HomePageTest(TestCase):
+    def setUp(self):
+        self.resp = self.client.get('/')
+    
+    def test_get(self):
+        'GET / must return status code 200'
+        self.assertEqual(200, self.resp.status_code)
+
+    def test_template(self):
+        'Homepage must use template core/index.tml'
+        self.assertTemplateUsed(self.resp, 'core/index.html')
+
+
+class AboutTest(TestCase):
+    def setUp(self):
+        self.resp = self.client.get('/about/')
+
+    def test_get(self):
+        'GET /about/ must return status code 200'
+        self.assertEqual(200, self.resp.status_code)
+
+    def test_template(self):
+        'About must use template core/about.html'
+        self.assertTemplateUsed(self.resp, 'core/about.html')
+
+
+class ContactTest(TestCase):
+    def setUp(self):
+        self.resp = self.client.get('/contact/')
+
+    def test_get(self):
+        'GET /contact/ must return status code 200'
+        self.assertEqual(200, self.resp.status_code)
+
+    def test_template(self):
+        'About must use template core/contact.html'
+        self.assertTemplateUsed(self.resp, 'core/contact.html')
+
+
--- collopyprobr/urls.py
+++ collopyprobr/urls.py
@@ -3,9 +3,9 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-
 urlpatterns = patterns('collopyprobr.core.views',
         url(r'^$', 'homepage', name='home'),
-        # url(r'^blog/', include('blog.urls')),
+        url(r'^about/', 'about', name='about'),
+        url(r'^contact/', 'contact', name='contact'), 
         url(r'^admin/', include(admin.site.urls)),
-)
+        )
